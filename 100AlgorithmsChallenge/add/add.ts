9bcd3760eeab0ff99eb6580528cdd65da3ee9188
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-03 16:37:43 -0400

    add

--- 100AlgorithmsChallenge/add/add.ts
+++ 100AlgorithmsChallenge/add/add.ts
@@ -1,13 +1,16 @@
 function add(param1: number, param2: number): number {
 
+    return param1 + param2
 }
 
-// function add2(param1: number[]): number {
+function add2(...param1: number[]): number {
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+    return param1.reduce((a,b) =>  a + b)
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+}
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
 
