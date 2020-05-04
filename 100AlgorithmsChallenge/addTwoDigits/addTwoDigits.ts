4b8581157269773baf598095cfb2f12ead12f2f8
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-04 03:17:36 -0400

    add two numbers

--- 100AlgorithmsChallenge/addTwoDigits/addTwoDigits.ts
+++ 100AlgorithmsChallenge/addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,13 @@
 function addTwoDigits(n: any): number {
 
+  let string = n.toString()
+  let split = string.split('')
+  
+  return split.reduce((a,b) => {
+      return parseInt(a) + parseInt(b)
+  }, 0)
+
+
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
