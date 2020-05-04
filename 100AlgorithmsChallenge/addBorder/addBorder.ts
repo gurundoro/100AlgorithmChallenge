8cd406c372d763531c456faddd5f52f037080020
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-04 03:08:54 -0400

    add border

--- 100AlgorithmsChallenge/addBorder/addBorder.ts
+++ 100AlgorithmsChallenge/addBorder/addBorder.ts
@@ -1,5 +1,16 @@
 function addBorder(picture: string[]): string[] {
+    let wall= '*'.repeat(picture[0].length + 2)
+   
+    
+    picture.unshift(wall)
+    picture.push(wall)
+    
+    for(let i = 0; i < picture.length; i++){
+        picture[i] = '*' + picture[i] + '*'
+    }
+
+    return picture
 
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
