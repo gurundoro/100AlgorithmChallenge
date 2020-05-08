afad88be489b7f8e07b73ef7aec3c4ee8aba0052
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-08 18:31:24 -0400

    all longest strings

--- 100AlgorithmsChallenge/allLongestStrings/allLongestStrings.ts
+++ 100AlgorithmsChallenge/allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,13 @@
 function allLongestStrings(inputArray: string[]): string[] {
 
+    let length = 0 
+    for(let i = 0; i < inputArray.length; i++){
+        if(length < inputArray[i].length){
+            length = inputArray[i].length
+        }
+    }
+
+    return inputArray.filter(element => element.length === length)
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
