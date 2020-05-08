57a300c767cbf9dcf014109d76a762eda654db8c
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-08 18:00:46 -0400

    adjacent Elements

--- 100AlgorithmsChallenge/adjacentElementsProduct/adjacentElementsProduct.ts
+++ 100AlgorithmsChallenge/adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,13 @@
 function adjacentElementsProduct(inputArray: number[]): number {
 
+    let max = 0 
+
+    for(let i = 0; i < inputArray.length -1; i++){
+        console.log
+        max = Math.max(max, (inputArray[i] * inputArray[i+1]) )
+    }
+
+    return max
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
