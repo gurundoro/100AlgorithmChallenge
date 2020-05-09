029a57634cd7f2d98031bdb0e1810aa3258703ec
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-09 03:26:35 -0400

    alternating sums solution

--- 100AlgorithmsChallenge/alternatingSums/alternatingSums.ts
+++ 100AlgorithmsChallenge/alternatingSums/alternatingSums.ts
@@ -1,5 +1,25 @@
 function alternatingSums(a: number[]): number[] {
 
+    let aEven = 0
+    let aOdd = 0
+
+    for(let i = 0; i < a.length; i++){
+        if(i % 2 === 0){
+            aEven += a[i]
+        } else {
+            aOdd += a[i]
+        }
+    }
+
+    // let one = aEven.reduce((acc, curr) => {
+    //     return acc + curr
+    // },0)
+    // let two = aOdd.reduce((acc, curr) => {
+    //     return acc + curr
+    // },0)
+
+    return [aEven, aOdd]
+
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
