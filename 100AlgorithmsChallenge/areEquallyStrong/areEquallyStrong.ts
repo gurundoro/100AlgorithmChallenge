33fdff02c93f3043fbf388629ca8a97a1d5adb72
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-09 03:39:45 -0400

    equally strong

--- 100AlgorithmsChallenge/areEquallyStrong/areEquallyStrong.ts
+++ 100AlgorithmsChallenge/areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,11 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
 
+    let me = yourLeft + yourRight
+    let you = friendsLeft + friendsRight
+
+    return me === you
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
