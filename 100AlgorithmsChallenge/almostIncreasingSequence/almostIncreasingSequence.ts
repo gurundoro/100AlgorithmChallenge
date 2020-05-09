561d37e359d40e1d27c40b1b5759fa7cc19253fa
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-08 21:30:30 -0400

    almost increasing sequence

--- 100AlgorithmsChallenge/almostIncreasingSequence/almostIncreasingSequence.ts
+++ 100AlgorithmsChallenge/almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,18 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
 
+    let count = 0
+    for(let i = 0; i < sequence.length-1; i++){
+        if(sequence[i] > sequence[i+1]){
+            count++
+        }
+    }
+    console.log(count)
+   
+    
+    return count <= 1
+
+  
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
