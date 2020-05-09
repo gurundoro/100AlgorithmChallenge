d1c1abdd3f676bc008e3c410a09598492dbe1ebd
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-08 22:25:10 -0400

    alphabetic shift

--- 100AlgorithmsChallenge/alphabeticShift/alphabeticShift.ts
+++ 100AlgorithmsChallenge/alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,20 @@
 function alphabeticShift(inputString: string): string {
 
+ let array = inputString.toLowerCase().split('')
+ let codeArray = []
+
+ console.log(array)
+
+for(let i = 0; i < array.length; i++){
+    if(array[i] === 'z'){
+       array[i] = 'a'
+       codeArray.push(array[i].charCodeAt(0))
+    } else {
+       codeArray.push(array[i].charCodeAt(0) + 1)
+    } 
+}
+ return codeArray.map(element => String.fromCharCode(element)).join('')
+
 }
 
 console.log(alphabeticShift('crazy'));
