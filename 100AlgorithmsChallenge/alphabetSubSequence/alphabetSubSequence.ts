abd3300983f3c75343c2a9d4d9f1ae06c014c5b5
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-08 23:04:47 -0400

    alphabet subsequence

--- 100AlgorithmsChallenge/alphabetSubSequence/alphabetSubSequence.ts
+++ 100AlgorithmsChallenge/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,14 @@
 function alphabetSubsequence(s: string): boolean {
+     
+    let array = s.split('').map(element => element.charCodeAt(0))
 
+    for(let i = 0; i < array.length; i++){
+        if(array[i] >= array[i+1]){
+            return false
+        }
+    }
+
+    return true
 }
 
 console.log(alphabetSubsequence('zab'))
