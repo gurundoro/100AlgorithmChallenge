fb09840eca1330edd4f63d8cefee6722cc9d7c9c
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-10 02:56:27 -0400

    testing

--- 100AlgorithmsChallenge/arrayConversion/arrayConversion.ts
+++ 100AlgorithmsChallenge/arrayConversion/arrayConversion.ts
@@ -1,5 +1,11 @@
 function arrayConversion(inputArray: number[]): number {
 
+    let result = [...inputArray]
+
+    for(let i = 0; i < inputArray; i++){
+        
+    }
+
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
