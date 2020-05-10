5f2607d29758d728b67439b3a841754b05998c1d
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-09 21:30:18 -0400

    array change

--- 100AlgorithmsChallenge/arrayChange/arrayChange.ts
+++ 100AlgorithmsChallenge/arrayChange/arrayChange.ts
@@ -1,4 +1,15 @@
 function arrayChange(inputArray: number[]): number {
+    let count = 0 
+
+    for(let i = 0; i < inputArray.length; i++){
+        if(inputArray[i] >= inputArray[i+1]){
+            let difference = (inputArray[i] + 1) - inputArray[i+1] 
+            inputArray[i + 1] = inputArray[i] + 1
+            count += difference
+        }
+    }
+
+    return count
  
 }
 
