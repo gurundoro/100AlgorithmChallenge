25617f50580c3789a6dd9fc11b5fd998cc8d5c8e
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-12 21:00:35 -0400

    array maximal adajacent difference (use Math.abs)

--- 100AlgorithmsChallenge/arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ 100AlgorithmsChallenge/arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,16 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
 
+    let total = -Infinity
+    let difference = total
+
+    for(let i = 0; i < inputArray.length -1; i++){
+
+         total = Math.abs(inputArray[i] - inputArray[i+1])
+         difference = Math.max(difference, total)
+    }
+
+    return difference
+
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
