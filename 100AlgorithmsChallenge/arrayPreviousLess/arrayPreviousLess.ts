3af199cf42b6e655f5c5f9e78d199dbdf435fd53
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-18 09:28:33 -0400

    arrayPreviousLess

--- 100AlgorithmsChallenge/arrayPreviousLess/arrayPreviousLess.ts
+++ 100AlgorithmsChallenge/arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,20 @@
 function arrayPreviousLess(items: number[]): number[] {
 
+    let newArray = []
+
+    for(let i = items.length-1; i >= 0; i--){
+        for(let j = i; j >= 0; j--){
+            if(items[i] > items[j]){
+                newArray.unshift(items[j])
+                break
+            }else if(j === 0){
+                newArray.unshift(-1)
+            }
+        }
+    }
+
+    return newArray
+
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
