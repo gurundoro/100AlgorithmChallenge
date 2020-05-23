26be86b61910fa0236ddef732e3db7025206ef3b
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-23 16:35:37 -0400

    case insensitive palindrome

--- 100AlgorithmsChallenge/caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ 100AlgorithmsChallenge/caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,5 +1,20 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
+     
+    let string = inputString.toLowerCase().split('')
 
+    let start = 0
+    let end = string.length-1
+
+    while(start < end){
+      if(string[start] === string[end]){
+          start++
+          end--
+      } else{
+          return false
+      }
+    }
+
+    return true
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
