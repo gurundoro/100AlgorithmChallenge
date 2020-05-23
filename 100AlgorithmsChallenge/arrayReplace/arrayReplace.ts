f532147a97952d1094d28e54ed490dd8867a7717
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-23 07:02:57 -0400

    array replace second solution

--- 100AlgorithmsChallenge/arrayReplace/arrayReplace.ts
+++ 100AlgorithmsChallenge/arrayReplace/arrayReplace.ts
@@ -1,16 +1,24 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
      
-     let newArray = []
+//      let newArray = []
 
-    for(let i = 0; i < inputArray.length; i++){
-        if(inputArray[i] === elemToReplace){
-           newArray.push(substitutionElem)
-        } else{
-           newArray.push(inputArray[i])
+//     for(let i = 0; i < inputArray.length; i++){
+//         if(inputArray[i] === elemToReplace){
+//            newArray.push(substitutionElem)
+//         } else{
+//            newArray.push(inputArray[i])
+//         }
+//     }
+
+//    return newArray
+        
+        for(let i = 0; i < inputArray.length; i++){
+            if(inputArray[i] === elemToReplace){
+                inputArray.splice(i, 1, substitutionElem)
+            }
         }
-    }
 
-   return newArray
+        return inputArray
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
