00d07aafd62694fcb1a63fa888c04119a7c2a226
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-23 13:32:54 -0400

    avoid obstacles

--- 100AlgorithmsChallenge/avoidObstacles/avoidObstacles.ts
+++ 100AlgorithmsChallenge/avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,27 @@
 function avoidObstacles(inputArray: number[]): number {
 
+   inputArray.sort((a,b) => a - b)
+
+    let jumpDistance = 1 
+
+    let obstacleHit = true 
+
+    while(obstacleHit){
+
+        obstacleHit = false
+        jumpDistance++
+
+        for(let i = 0; i < inputArray.length; i++){
+            if(inputArray[i] % jumpDistance === 0){
+                obstacleHit = true
+                break
+            }
+        }
+     }
+
+     return jumpDistance
+    }
+
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
