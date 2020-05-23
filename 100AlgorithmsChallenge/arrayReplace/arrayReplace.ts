658997e02e480cbdd6e40e5fe00781c110e4d053
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-23 07:00:21 -0400

    replace array first solution

--- 100AlgorithmsChallenge/arrayReplace/arrayReplace.ts
+++ 100AlgorithmsChallenge/arrayReplace/arrayReplace.ts
@@ -1,5 +1,16 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+     
+     let newArray = []
+
+    for(let i = 0; i < inputArray.length; i++){
+        if(inputArray[i] === elemToReplace){
+           newArray.push(substitutionElem)
+        } else{
+           newArray.push(inputArray[i])
+        }
+    }
+
+   return newArray
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
