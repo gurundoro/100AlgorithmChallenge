6232d9f34fc85bbe0f5b64e329bdb3f1a400cce2
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-24 11:40:13 -0400

    common characters

--- 100AlgorithmsChallenge/commonCharacterCount/commonCharacterCount.ts
+++ 100AlgorithmsChallenge/commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,28 @@
 function commonCharacterCount(s1: string, s2: string): number {
-   
+    
+    let hash = {}
+    let firstString = s1.split("")
+    let secondString = s2.split("")
+
+    for(let i = 0; i < firstString.length; i++){
+        if(hash[firstString[i]]){
+            hash[firstString[i]] += 1
+        } else{
+            hash[firstString[i]] = 1
+        }
+    }
+
+    let count = 0 
+    console.log(hash)
+
+    for(let j = 0; j < secondString.length; j++){
+        if(hash[secondString[j]] && hash[secondString[j]] > 0){
+            count++
+            hash[secondString[j]] -= 1
+        }
+    }
+
+    return count
 }
 
 console.log(commonCharacterCount('aabcc', 'adcaa'));
