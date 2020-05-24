cbfc5f2fcf2408d93444b07903037874cce25f8a
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-24 15:48:58 -0400

    compareIntergers

--- 100AlgorithmsChallenge/compareIntegers/compareIntegers.ts
+++ 100AlgorithmsChallenge/compareIntegers/compareIntegers.ts
@@ -1,5 +1,16 @@
 function compareIntegers(a: string, b: string): string {
 
+    let number1 = parseInt(a)
+    let number2 = parseInt(b)
+
+    if(a > b){
+        return "greater"
+    }else if(a < b){
+        return "less"
+    }else{
+        return "equal"
+    }
+
 }
 
 console.log(compareIntegers('12', '13'));
