d674d88917ae25e3f50a99790f2a4ec33585f196
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-30 15:20:25 -0400

    solution

--- 100AlgorithmsChallenge/composeRanges/composeRanges.ts
+++ 100AlgorithmsChallenge/composeRanges/composeRanges.ts
@@ -2,7 +2,32 @@ function composeRanges(nums: number[]): string[] {
 
      let res = []
 
+     let range = ''
+
+    
+
+    for(let i = 0; i < nums.length; i++){
+        range = `${nums[i]}`
+        if(i === nums.length-1){
+            res.push(range)
+            break
+        }
+        for(let j = i; j <nums.length; j++){
+            if(nums[j]+1 !== nums[j+1]){
+                range += `->${nums[j]}`
+                res.push(range)
+                i = j
+                break    
+            }
+           
+            console.log(range)  
+            
+        }
+        console.log(range)
+    }
      
+
+    return res
 
 }
 
