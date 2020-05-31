1d72f26cb30741b4e3457448b9434999caedae34
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-30 23:05:02 -0400

    composeRanges with explanantion

--- 100AlgorithmsChallenge/composeRanges/composeRanges.ts
+++ 100AlgorithmsChallenge/composeRanges/composeRanges.ts
@@ -1,32 +1,40 @@
 function composeRanges(nums: number[]): string[] {
 
+    //set result array 
      let res = []
 
+    //set range string
      let range = ''
 
     
-
+    //start loop at beginning
     for(let i = 0; i < nums.length; i++){
+        //set the beginning of the range 
         range = `${nums[i]}`
+        //if range is the last number add to array and break 
         if(i === nums.length-1){
             res.push(range)
             break
         }
+        //iterate through array
         for(let j = i; j <nums.length; j++){
+            //if the number is not one less the the next number
             if(nums[j]+1 !== nums[j+1]){
+                //complete range string 
                 range += `->${nums[j]}`
+                //add range string to array
                 res.push(range)
+                //set index to last choosen element
                 i = j
+                //break loop
                 break    
             }
-           
-            console.log(range)  
             
         }
-        console.log(range)
+
     }
      
-
+    //return result 
     return res
 
 }
