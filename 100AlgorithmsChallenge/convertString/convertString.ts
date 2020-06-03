3ab8944867bbf0d7a0b2cddaa6b6c6e66634640c
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-03 15:14:31 -0400

    convert string

--- 100AlgorithmsChallenge/convertString/convertString.ts
+++ 100AlgorithmsChallenge/convertString/convertString.ts
@@ -1,5 +1,20 @@
 function convertString(s: string, t: string): boolean {
 
+    let word = ''
+    let string = s.split('')
+    let index = 0 
+
+    for(let i = 0; i < string. length; i++){
+        if(string[i] === t[index]){
+            word += string[i]
+            index++
+
+            if(word === t){
+                return true
+            }
+        }
+    }
+    return false
 }
 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
