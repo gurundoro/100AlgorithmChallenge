b39c18b760faa01bed58ada815399059dd578a7d
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-12 08:23:49 -0400

    deposit profit

--- 100AlgorithmsChallenge/depositProfit/depositProfit.ts
+++ 100AlgorithmsChallenge/depositProfit/depositProfit.ts
@@ -1,5 +1,21 @@
 function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+
+    let years = 0 
+
+    let percentage = rate / 100
+
+    while(threshold > deposit){
+
+        if(threshold > deposit){
+        let intrest = deposit * percentage
+
+        deposit += intrest
+        }
+
+        years++
+
+    }
+    return years
 }
 
 console.log(depositProfit(100, 20, 170))
