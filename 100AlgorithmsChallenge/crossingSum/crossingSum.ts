bf12153ff4c607c64c041ed06719230e8f5211fc
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-11 22:54:16 -0400

    crossing sum solution;

--- 100AlgorithmsChallenge/crossingSum/crossingSum.ts
+++ 100AlgorithmsChallenge/crossingSum/crossingSum.ts
@@ -1,7 +1,18 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
-  
- //another day 
- //another dollar
+    
+    //find total of row by adding via reduce 
+    const row =  matrix[a].reduce((a ,b) => a + b)
+    let column = 0 
+    
+    //find total of column by iterating over array elements
+    for(let i = 0; i < matrix.length; i++){
+    //if element matches a add 0 to column 
+    //if not add integer in the specified column of that row
+        column += i !== a ? matrix[i][b]:0
+    }
+    
+    //add column and row totals
+    return row + column
 }
 
 console.log(crossingSum([[1, 1, 1, 1], 
