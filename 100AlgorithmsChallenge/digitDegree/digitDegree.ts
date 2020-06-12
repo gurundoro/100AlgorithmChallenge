78c0c1e76ab822bdc5c1eafbbd7d67dd80251b69
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-12 11:41:17 -0400

    digitDegree solution

--- 100AlgorithmsChallenge/digitDegree/digitDegree.ts
+++ 100AlgorithmsChallenge/digitDegree/digitDegree.ts
@@ -1,5 +1,22 @@
 function digitDegree(n: number): number {
   
+
+  let numToArray = n.toString().split('')
+
+ degree  = 0 
+  if(numToArray.length === 1){
+      return degree
+  }
+    
+  n = reduced(numToArray)
+  digitDegree(n)
+  degree ++
+  return degree
+
+}
+
+function reduced(array){
+    return array.reduce((a,b) => Number(a) + Number(b))
 }
 
 console.log(digitDegree(5));
