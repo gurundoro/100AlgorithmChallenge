2c6e965e89891ccd1be7c52268f87f3dc73c3d09
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-12 08:29:56 -0400

    different symbols

--- 100AlgorithmsChallenge/differentSymbolsNaive/differentSymbolsNaive.ts
+++ 100AlgorithmsChallenge/differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,10 @@
 function differentSymbolsNaive(s: string): number {
- 
+ //split string 
+ let sA = s.split('')
+ //create set from array
+ let map = new Set(sA)
+ //return set size
+ return map.size
 }
 
 console.log(differentSymbolsNaive('cabca'));
