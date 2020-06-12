08353ebd690eb52c6a7be6c6260f0ee636dae69c
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-12 08:34:17 -0400

    different symbols naive solution 2

--- 100AlgorithmsChallenge/differentSymbolsNaive/differentSymbolsNaive.ts
+++ 100AlgorithmsChallenge/differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,10 +1,18 @@
 function differentSymbolsNaive(s: string): number {
  //split string 
  let sA = s.split('')
- //create set from array
- let map = new Set(sA)
- //return set size
- return map.size
+//  //create set from array
+//  let map = new Set(sA)
+//  //return set size
+//  return map.size
+
+    let map = {}
+    for(let i = 0; i < sA.length; i++){
+    map[sA[i]]? map[sA[i]]++: map[sA[i]] = 1
+    }
+
+   return Object.keys(map).length
+
 }
 
 console.log(differentSymbolsNaive('cabca'));
