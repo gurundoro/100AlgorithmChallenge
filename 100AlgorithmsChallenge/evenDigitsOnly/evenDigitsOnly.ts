2b198246a0635d66f3a7535d9d8f30ddd3b13bff
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 19:35:04 -0400

    even digits only

--- 100AlgorithmsChallenge/evenDigitsOnly/evenDigitsOnly.ts
+++ 100AlgorithmsChallenge/evenDigitsOnly/evenDigitsOnly.ts
@@ -1,4 +1,10 @@
 function evenDigitsOnly(n: number): boolean {
+    
+    let string = n.toString().split('')
+    const isEven = (value) => value % 2 === 0
+
+
+    return string.every(isEven)
 
 }
 
