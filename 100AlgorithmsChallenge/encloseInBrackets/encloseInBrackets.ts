7118ea34b3eb2f4069f1b212356b4eea80086ee9
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 19:28:44 -0400

    enclose in brackets

--- 100AlgorithmsChallenge/encloseInBrackets/encloseInBrackets.ts
+++ 100AlgorithmsChallenge/encloseInBrackets/encloseInBrackets.ts
@@ -1,5 +1,11 @@
 function encloseInBrackets(inputString: string): string {
+  
+    let split = inputString.split('')
 
+    split.push(")")
+    split.unshift("(")
+
+    return split.join('')
 }
 
 console.log(encloseInBrackets('abacaba'));
