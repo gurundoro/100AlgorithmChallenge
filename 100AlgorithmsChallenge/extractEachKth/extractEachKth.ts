09380c2f967db2a20c903b4efd6c4c0a7eaf71b3
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 19:51:54 -0400

    remove kth element second solution

--- 100AlgorithmsChallenge/extractEachKth/extractEachKth.ts
+++ 100AlgorithmsChallenge/extractEachKth/extractEachKth.ts
@@ -1,15 +1,17 @@
 function extractEachKth(inputArray:number[], k: number): number[] {
 
-    count = 0
-    for(let i = 0; i < inputArray.length; i++){
-        count++
-        if(count === k){
-            inputArray.splice(i, 1)
-            count = 1
-        }
-    }
+    // count = 0
+    // for(let i = 0; i < inputArray.length; i++){
+    //     count++
+    //     if(count === k){
+    //         inputArray.splice(i, 1)
+    //         count = 1
+    //     }
+    // }
 
-    return inputArray
+    // return inputArray
+
+    return inputArray.filter((input, index) => (index + 1) % k !== 0)
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
