6ea83f02822ea748a8fc3588d664c40d7e1527cd
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 01:36:36 -0400

    domain types solution

--- 100AlgorithmsChallenge/domainType/domainType.ts
+++ 100AlgorithmsChallenge/domainType/domainType.ts
@@ -1,7 +1,21 @@
 function domainType(domains: string[]): string[] {
- //testing testing one two onw two
+    
+    let domain = {
+        com:'commercial',
+        org:'organization',
+        net: 'network',
+        info: 'information'
+    }
 
- //another line for good measure
+    let labels = []
+
+    for(let i = 0; i < domains.length; i++){
+        let topLevel = domains[i].split('.')
+        
+        labels.push(domain[topLevel[topLevel.length-1]])
+    }
+
+    return labels
 }
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
