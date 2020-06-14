74762c6e30c388b670c1bb89ce0403473d5486c9
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 16:30:05 -0400

    in the running with comments

--- 100AlgorithmsChallenge/electionWinners/electionWinners.ts
+++ 100AlgorithmsChallenge/electionWinners/electionWinners.ts
@@ -1,19 +1,27 @@
 function electionsWinners(votes: number[], k: number): number {
 
+   //set return value
    let inTheRunning = 0
+   //find largest vote from array
    const mostVotes = Math.max(...votes)
+   //sort votes
    const sortedVotes = votes.sort((a,b) => b-a)
-
+    
+   //if first and second value are not eqaul and there are no votes left return 1
     if(sortedVotes[0] !== sortedVotes[1] && k === 0){
         return 1 
     }
-
+    
+    //iterate over votes
     votes.forEach((voteCount) => {
+    //if the difference between vote most votes and current vote is less than votes left 
         if(mostVotes - voteCount < k){
+    //increase in the running
             inTheRunning++
         }
     })
-
+   
+    //return inTheRunning 
     return inTheRunning
 
 }
