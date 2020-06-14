6df0e449119369974f55621b92d68c77c48581ac
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 19:30:25 -0400

    enclose In brackets

--- 100AlgorithmsChallenge/encloseInBrackets/encloseInBrackets.ts
+++ 100AlgorithmsChallenge/encloseInBrackets/encloseInBrackets.ts
@@ -1,10 +1,15 @@
 function encloseInBrackets(inputString: string): string {
-  
+    
+    //turn string to array
     let split = inputString.split('')
-
+    
+    //add closing bracket to the back
     split.push(")")
-    split.unshift("(")
 
+    //add openeing bracket to the front 
+    split.unshift("(")
+    
+    //joint string 
     return split.join('')
 }
 
