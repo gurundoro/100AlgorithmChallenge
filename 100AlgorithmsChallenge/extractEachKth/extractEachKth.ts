b6e7a4f7fdf1ec78e12fd8fd1ad34fd2adce84be
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 19:49:06 -0400

    extract each kth element

--- 100AlgorithmsChallenge/extractEachKth/extractEachKth.ts
+++ 100AlgorithmsChallenge/extractEachKth/extractEachKth.ts
@@ -1,4 +1,15 @@
 function extractEachKth(inputArray:number[], k: number): number[] {
+
+    count = 0
+    for(let i = 0; i < inputArray.length; i++){
+        count++
+        if(count === k){
+            inputArray.splice(i, 1)
+            count = 1
+        }
+    }
+
+    return inputArray
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
