524b0cd965b2217a50b50e7a244a1ddb22ee4f72
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 19:58:21 -0400

    extract matrix column

--- 100AlgorithmsChallenge/extractMatrixColumn/extractMatrixColumn.ts
+++ 100AlgorithmsChallenge/extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,13 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
 
+    let columnArray = []
+
+    for(let i = 0; i < matrix.length; i++){
+        columnArray.push(matrix[i][column])
+    }
+
+
+    return columnArray
 }
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
