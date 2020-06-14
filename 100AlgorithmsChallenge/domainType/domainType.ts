a309508bf7b7886aa6a6c8f70d0f0617d428c9ab
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-13 23:59:00 -0400

    testing

--- 100AlgorithmsChallenge/domainType/domainType.ts
+++ 100AlgorithmsChallenge/domainType/domainType.ts
@@ -1,5 +1,7 @@
 function domainType(domains: string[]): string[] {
  //testing testing one two onw two
+
+ //another line for good measure
 }
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
