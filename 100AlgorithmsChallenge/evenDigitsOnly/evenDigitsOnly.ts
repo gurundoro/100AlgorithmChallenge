5c236d8713b5623739134acb525c70e5c63edbce
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 19:41:27 -0400

    even digits only with notes

--- 100AlgorithmsChallenge/evenDigitsOnly/evenDigitsOnly.ts
+++ 100AlgorithmsChallenge/evenDigitsOnly/evenDigitsOnly.ts
@@ -1,9 +1,11 @@
 function evenDigitsOnly(n: number): boolean {
-    
+    //turn number to string to array 
     let string = n.toString().split('')
+
+    //create function to check if its even  
     const isEven = (value) => value % 2 === 0
 
-
+    //return boolean using created function and every 
     return string.every(isEven)
 
 }
