2dbab924e2d5894a6158b1fc49344b70fdb24f25
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 20:03:33 -0400

    factorialize number

--- 100AlgorithmsChallenge/factorializeANumber/factorializeANumber.ts
+++ 100AlgorithmsChallenge/factorializeANumber/factorializeANumber.ts
@@ -1,5 +1,10 @@
 function factorializeANumber(num: number): number {
+   
+    if(num === 1){
+        return 1
+    }
 
+    return num * factorializeANumber(num - 1)
 }
 
 console.log(factorializeANumber(5));
