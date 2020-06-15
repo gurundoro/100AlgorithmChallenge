1b3c7f57948a41e37bc1613bfc6460e7861f7c22
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-14 20:24:27 -0400

    fancy rides

--- 100AlgorithmsChallenge/fancyRide/fancyRide.ts
+++ 100AlgorithmsChallenge/fancyRide/fancyRide.ts
@@ -1,5 +1,19 @@
 function fancyRide(l: number, fares: number[]): string {
+  
+ let rides = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"]
 
+ let max = 0 
+ let index = 0 
+
+ for(let i = 0; i < fares.length; i++){
+    let total = l * fares[i]
+    max =  Math.max(max, total)
+    if(total < 20 && total === max){
+      index = i 
+    }
+ }
+
+ return rides[index]
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
