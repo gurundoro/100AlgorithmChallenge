07b90d7fe42d53458adb3e4d7306b8f2789ad798
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-16 23:35:38 -0400

    findClosestPair first attempt

--- 100AlgorithmsChallenge/findClosestPair/findClosestPair.ts
+++ 100AlgorithmsChallenge/findClosestPair/findClosestPair.ts
@@ -1,5 +1,29 @@
 function findClosestPair(numbers: number[], sum: number): number {
+    
+    let map = {}
+
+    let  distance = Infinity
+
+
+    for(let i = 0; i < numbers.length; i++){
+        map[numbers[i]] = i 
+    }
+
+    
+
+    for(let i = 0; i < numbers.length; i++){
+       for(let j = 0; j < numbers.length; j++){
+           if(numbers[i] + map[j] === sum){
+              let space = Math.abs(i - j)
+                 distance = Math.min(space,distance )
+           }
+       }
+    }
+
    
+      if(distance === Infinity) return -1 
+
+    return distance
 }
 
 console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
