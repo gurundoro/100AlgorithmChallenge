89e303961361ffc6d97b7b955f528cccf9565da4
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-17 00:57:04 -0400

    first duplicate

--- 100AlgorithmsChallenge/firstDuplicate/firstDuplicate.ts
+++ 100AlgorithmsChallenge/firstDuplicate/firstDuplicate.ts
@@ -1,5 +1,16 @@
 function firstDuplicate(a: number[]): number {
     
+    let map = {}
+
+    for(let i = 0; i < a.length; i++){
+        if(map[a[i]]){
+            return  a[i]
+        }else{
+            map[a[i]] = true
+        }
+    }
+
+    return -1 
 }
 
 console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
