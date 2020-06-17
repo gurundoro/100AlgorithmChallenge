92cae9e212d8ff1ed31835a9cf4880847a6b3f08
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-17 00:47:49 -0400

    find email domian

--- 100AlgorithmsChallenge/findEmailDomain/findEmailDomain.ts
+++ 100AlgorithmsChallenge/findEmailDomain/findEmailDomain.ts
@@ -1,5 +1,10 @@
 function findEmailDomain(address: string): string {
+ 
+    let email = address.split('')
 
+    let index = email.lastIndexOf('@')
+
+    return email.slice(index).join('')
 }
 
 console.log(findEmailDomain('prettyandsimple@example.com'));
