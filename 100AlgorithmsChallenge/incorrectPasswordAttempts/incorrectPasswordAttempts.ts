c024865ae42fcaca7b1aad103f2a029b23fc16dc
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-19 03:23:14 -0400

    incorrect password attempts

--- 100AlgorithmsChallenge/incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ 100AlgorithmsChallenge/incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,22 @@
 function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
 
+    let results = true 
+    let count = 0 
+
+    for(let i = 0; i < attempts.length; i++){
+        if(attempts[i] === passcode){
+            count = 0 
+        } else {
+            count++
+        }
+
+        if(count === 10){
+            return true
+        }
+    }
+
+    return false
+
 }
 
 console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
