f91a4b5c866723be9cfaf59c6db3c08dd9a8d4cc
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 04:56:42 -0400

    lateRide

--- 100AlgorithmsChallenge/largestOfFour/largestOfFour.ts
+++ 100AlgorithmsChallenge/largestOfFour/largestOfFour.ts
@@ -1,5 +1,13 @@
 function largestOfFour(nums: number[][]): number[] {
 
+    let largestArray = []
+
+    for(let i = 0; i < nums.length;i++){
+        largestArray.push(Math.max(...nums[i]))
+    }
+
+    return largestArray
+
 }
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
--- 100AlgorithmsChallenge/lateRide/lateRide.ts
+++ 100AlgorithmsChallenge/lateRide/lateRide.ts
@@ -1,5 +1,20 @@
 function lateRide(n: number): number {
 
+    let hours = n/60
+    let time = hours.toFixed(2).toString().split('')
+
+    console.log(808/60)
+
+    let number = time.filter(a => a !== '.').map(Number)
+    console.log(number)
+    
+    return number.reduce((a, b) => {
+      {
+        return a + b
+      }
+     },0 
+    )
+
 }
 
 console.log(lateRide(240));
