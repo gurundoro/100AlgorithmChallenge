2e164bb2b6d415013efa9d0e937d3694f0232d60
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 06:39:26 -0400

    makeArrayConsecutive'

--- 100AlgorithmsChallenge/makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ 100AlgorithmsChallenge/makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,17 @@
 function makeArrayConsecutive2(statues: number[]): number {
 
+    let sorted = statues.sort((a,b) => a - b)
+
+    let count = 0
+
+    for(let i = 0; i < sorted.length;i++){
+        if(sorted[i+1] !== sorted[i] + 1){
+            count++
+        }
+    }
+
+    return count++
+
 }
 
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
