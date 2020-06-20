83d252a1c721ead78bc6e44d3400d9c999503ffd
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 09:43:55 -0400

    mineSweeper

--- 100AlgorithmsChallenge/mineSweeper/mineSweeper.ts
+++ 100AlgorithmsChallenge/mineSweeper/mineSweeper.ts
@@ -1,6 +1,45 @@
 function minesweeper(matrix: boolean[][]): number[][] {
   
-}
+    var newArr = [];
+
+    for(var i=0; i < matrix.length; i++){
+      newArr.push([]);
+      for(var j=0; j < matrix[i].length; j++){
+        var cnt = 0;
+        if(i>0){
+          if(matrix[i-1][j]){
+            cnt += 1;
+          }
+          if(matrix[i-1][j+1]){
+            cnt += 1;
+          }
+          if(matrix[i-1][j-1]){
+            cnt += 1;
+          }
+        }
+        if(i < matrix.length - 1){
+          if(matrix[i+1][j]){
+            cnt +=1;
+          }
+          if(matrix[i+1][j+1]){
+            cnt += 1;
+          }
+          if(matrix[i+1][j-1]){
+            cnt +=1;
+          }
+        }
+  
+        if(matrix[i][j+1]){
+          cnt +=1;
+        }
+        if(matrix[i][j-1]){
+          cnt +=1;
+        }
+        newArr[i].push(cnt)
+      }
+    }
+    return newArr
+  }
 
 console.log(minesweeper([[true, false, false],
 [false, true, false],
