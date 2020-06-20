7d41b521287d5e84a38ec06462838564379db87a
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 02:53:04 -0400

    integer to fixed width with comments

--- 100AlgorithmsChallenge/integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
+++ 100AlgorithmsChallenge/integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
@@ -1,5 +1,26 @@
 function integerToStringOfFixedWidth(number: number, width: number): string {
-  
+    
+    //turn number toString
+    let string = number.toString()
+    //find length of String 
+    let length = string.length
+    
+    //find index to cut string from if less than length of string
+    let cut =  length - width 
+
+    //find amount of zeroes to add if greater than string 
+    let zeroes = width - length
+    
+    //check less than or equal to length
+    if(cut >= 0){
+        //return cut 
+        return string.substring(cut)
+     //else return string with additional zeroes 
+    }else {
+       let zString = '0'.repeat(zeroes)
+       return zString.concat(string)
+    }
+   
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));
