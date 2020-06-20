8eb8c4478d5dab23b2b26d07f4af92033ce80124
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 02:59:15 -0400

    isLucky

--- 100AlgorithmsChallenge/isLucky/isLucky.ts
+++ 100AlgorithmsChallenge/isLucky/isLucky.ts
@@ -1,5 +1,20 @@
 function isLucky(n: number): boolean {
+
+    let array = n.toString().split('')
+    let firstHalf = array.slice(0, array.length/2)
+    let secondHalf = array.slice(array.length/2)
+
+    if(sum(firstHalf) === sum(secondHalf)){
+        return true
+    }
+    console.log(sum(firstHalf))
+
+    return false
     
+}
+
+function sum (array){
+    return array.reduce((a,b) => Number(a) + Number(b))
 }
 
 console.log(isLucky(1230));
