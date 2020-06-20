593f98c4aa89da9b011cb503c93fdea7db5edd27
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 03:38:00 -0400

    isTandemRepeat

--- 100AlgorithmsChallenge/isTandemRepeat/isTandemRepeat.ts
+++ 100AlgorithmsChallenge/isTandemRepeat/isTandemRepeat.ts
@@ -1,5 +1,25 @@
 function isTandemRepeat(inputString: string): boolean {
-  
+    //find the length of the string
+    let word = inputString.length
+
+    //check if string length is even
+    if( word % 2 === 0){
+        //split string into first half
+        let halfword = inputString.slice(0, word/2)
+        //split string into second half
+        let secondhalf =  inputString.slice(word/2)
+
+        //check if halfs are equal
+        if(halfword === secondhalf){
+            //if so return true
+            return true
+        }
+    }
+    
+    
+    
+    //if string is  return false or unequal
+    return false
 }
 
 console.log(isTandemRepeat('tandemtandem'))
