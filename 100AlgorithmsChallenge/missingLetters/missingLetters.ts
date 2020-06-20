0ee892f0a53de74388e79c83b431c684831079fc
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 10:19:56 -0400

    missing letters

--- 100AlgorithmsChallenge/missingLetters/missingLetters.ts
+++ 100AlgorithmsChallenge/missingLetters/missingLetters.ts
@@ -1,4 +1,17 @@
 function missingLetters(str: string): string {
+
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let chars = str.split('')
+
+    if(chars[0] !== 'a'){
+        return undefined
+    }
+
+    for(let i = 0; i < chars.length; i++){
+        if(chars[i] !== alphabet[i]){
+            return alphabet[i]
+        }
+    }
    
 }
 console.log(missingLetters("bce"));
