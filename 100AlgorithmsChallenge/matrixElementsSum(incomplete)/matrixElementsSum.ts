613a0df3ae019e3ab6ca007f5c0c9c975e903c18
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 09:11:01 -0400

    matrixElementSum

--- 100AlgorithmsChallenge/matrixElementsSum(incomplete)/matrixElementsSum.ts
+++ 100AlgorithmsChallenge/matrixElementsSum(incomplete)/matrixElementsSum.ts
@@ -1,6 +1,26 @@
 function matrixElementsSum(matrix: any[][]): number {
-
-    let price 
+     //set price 
+     let priceTotal = 0;
+     //set banned index array
+     const bannedIndex = []
+     
+     //iterate over array elements
+     for(let i = 0; i < matrix.length; i++){
+         //iterate over individual array elements
+         for(let j = 0; j < matrix[i].length; j++){
+             //if element === 0 
+             if(matrix[i][j]===0){
+                 //remember index is occupied by ghosts
+                 bannedIndex.push(j)
+                 //or if index has yet to be been banned 
+             } else if(bannedIndex.indexOf(j) === -1){
+                 //add element 
+                 priceTotal += matrix[i][j]
+             }
+         }
+     }
+     //return total 
+     return priceTotal
 
 }
 
