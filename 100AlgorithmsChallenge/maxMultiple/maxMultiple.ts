d8cb8c311ba7b6065d3acec2290ad4519e1befff
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 09:22:03 -0400

    max multiplier

--- 100AlgorithmsChallenge/maxMultiple/maxMultiple.ts
+++ 100AlgorithmsChallenge/maxMultiple/maxMultiple.ts
@@ -1,5 +1,17 @@
 function maxMultiple(divisor: number, bound: number): number  {
 
+    let number = divisor
+
+    while(number <= bound){
+        if((number+divisor) > bound){
+            return number
+        } else{
+            number += divisor
+        }
+    }
+
+    return number
+
 }
 
 console.log(maxMultiple(3,10));
