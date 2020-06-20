1e7e43e1f9666eb7efbe47ccd3b7d712cc927fe8
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 05:04:29 -0400

    a better way to do late ride

--- 100AlgorithmsChallenge/lateRide/lateRide.ts
+++ 100AlgorithmsChallenge/lateRide/lateRide.ts
@@ -1,19 +1,30 @@
 function lateRide(n: number): number {
 
-    let hours = n/60
-    let time = hours.toFixed(2).toString().split('')
+    // let hours = n/60
+    // let time = hours.toFixed(2).toString().split('')
 
-    console.log(808/60)
+    // console.log(808/60)
 
-    let number = time.filter(a => a !== '.').map(Number)
-    console.log(number)
+    // let number = time.filter(a => a !== '.').map(Number)
+    // console.log(number)
     
-    return number.reduce((a, b) => {
-      {
-        return a + b
-      }
-     },0 
-    )
+    // return number.reduce((a, b) => {
+    //   {
+    //     return a + b
+    //   }
+    //  },0 
+    // )
+
+   let hours = Math.floor((n/60)).toString()
+   let minutes = (n % 60).toString()
+   console.log(minutes)
+
+   return hours.concat(minutes)
+                        .split('')
+                        .map(Number)
+                        .reduce((a,b) => a+b)
+
+
 
 }
 
