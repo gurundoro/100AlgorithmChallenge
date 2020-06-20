592fea9048e414a3eff877c0fd07b9c8ee76610f
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 06:44:42 -0400

    make array consecutive second solution

--- 100AlgorithmsChallenge/makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ 100AlgorithmsChallenge/makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,11 +1,12 @@
 function makeArrayConsecutive2(statues: number[]): number {
 
     let sorted = statues.sort((a,b) => a - b)
-
+    let min = sorted[0]
+    let max = sorted[sorted.length-1]
     let count = 0
 
-    for(let i = 0; i < sorted.length;i++){
-        if(sorted[i+1] !== sorted[i] + 1){
+    for(let i = min; i < max;i++){
+        if(statues.indexOf(i) === -1){
             count++
         }
     }
