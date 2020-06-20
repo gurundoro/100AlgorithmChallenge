02f3aaf9630d63a48b09c60b06948223532f0d40
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 05:33:39 -0400

    launchSequenceChecker

--- 100AlgorithmsChallenge/launchSequenceChecker/launchSequenceChecker.ts
+++ 100AlgorithmsChallenge/launchSequenceChecker/launchSequenceChecker.ts
@@ -1,5 +1,20 @@
 function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
 
+    let map = {}
+
+  for(let i = 0; i < systemNames.length; i++){
+      if(map.hasOwnProperty(systemNames[i])){
+         if(map[systemNames[i]] > stepNumbers[i])
+          return false
+      }else{
+          map[systemNames[i]] = stepNumbers[i]
+      }
+  }
+
+  console.log(map)
+
+  return true
+
 }
 
 console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
