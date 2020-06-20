d0c3b731c0f5a7844bac6a75ee9c5fc7b50dc8f5
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 05:41:37 -0400

    longest digits prefix

--- 100AlgorithmsChallenge/longestDigitsPrefix/longestDigitsPrefix.ts
+++ 100AlgorithmsChallenge/longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,21 @@
 function longestDigitsPrefix(inputString: string): string {
 
+    let array = inputString.split('')
+
+    let prefix = ''
+
+    for(let i = 0; i < array.length; i++){
+        if(Number(array[i]) <= 9){
+           
+            prefix += array[i]
+        }else{
+            return prefix
+        }
+    }
+   
+    return prefix
+ 
+
 }
 
 console.log(longestDigitsPrefix('123aa1'));
