b987a7b61ff6466d05443baebd5604fb5d0ac705
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-21 06:48:53 -0400

    sortByHeight

--- 100AlgorithmsChallenge/sortByHeight/sortByHeight.ts
+++ 100AlgorithmsChallenge/sortByHeight/sortByHeight.ts
@@ -1,5 +1,23 @@
 function sortByHeight(a: number[]): number[] {
 
+    let indices = []
+
+    for(let i = 0; i < a.length; i++){
+        if(a[i] === -1){
+            indices.push(i)
+        }
+    }
+
+    let heights = a.filter(a => a !== -1).sort((a,b) => a-b)
+
+    console.log(indices)
+
+    for(let i = 0; i < indices.length; i++){
+        heights.splice(indices[i],0, -1)
+    }
+
+    return heights
+
 }
 
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
