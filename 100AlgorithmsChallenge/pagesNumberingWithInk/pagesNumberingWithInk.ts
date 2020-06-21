e081f8b2c67e2fd33ffc0798ae9fe081ef7daa84
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 23:20:51 -0400

    check palindrome

--- 100AlgorithmsChallenge/pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ 100AlgorithmsChallenge/pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -2,7 +2,7 @@ function pagesNumberingWithInk(current: number, numberOfDigits: number): number 
     while(numberOfDigits >= current.toString().length){
         numberOfDigits -= current.toString().length
 
-        ifumberOfDigits >= current.toString().length){
+        if(numberOfDigits >= current.toString().length){
             current++
         }
     }
--- 100AlgorithmsChallenge/palindromeRearranging/palindromeRearranging.ts
+++ 100AlgorithmsChallenge/palindromeRearranging/palindromeRearranging.ts
@@ -1,4 +1,32 @@
 function palindromeRearranging(inputString: string): boolean {
+ //create map 
+ let letters = {}
+ //split string to populate map
+ let string = inputString.split('')
+ // initialize odd count
+ let oddCount = 0
+ 
+ //populate the map
+ for(let i = 0; i < string.length; i++){
+     if(letters.hasOwnProperty(string[i])){
+         letters[string[i]]++
+     } else{
+         letters[string[i]] = 1
+     }
+ }
+
+ //check the value of the object
+ for(let key in letters){
+     //if value is odd increase odd count
+     if(letters[key] % 2 !== 0){
+         oddCount++
+     }
+ }
+
+ //if more than one value is odd it is a palindrome or it it's not
+ return oddCount > 1? false: true
+
+
 
 }
 
