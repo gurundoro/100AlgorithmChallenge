dbc0e634fe21b2759426f83601445be1e7bfb159
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-21 06:23:43 -0400

    reverse string

--- 100AlgorithmsChallenge/reverseAString/reverseAString.ts
+++ 100AlgorithmsChallenge/reverseAString/reverseAString.ts
@@ -1,5 +1,22 @@
 function reverseAString(str: string): string {
 
+    // return str.split('').reverse().join('')
+
+    let reversed = ''
+
+    let oldStr = str.split('')
+
+    console.log(oldStr)
+
+    for(let i = oldStr.length-1; i >=0; i--){
+        reversed += oldStr[i]
+    }
+
+
+    return reversed
+
+    
+
 }
 
 console.log(reverseAString('hello'));
