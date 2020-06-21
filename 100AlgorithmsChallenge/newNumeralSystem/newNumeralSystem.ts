8a7aae39cb20813d41c21c86ce0dfba47f4a1b3a
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 21:49:24 -0400

    new numeral system

--- 100AlgorithmsChallenge/newNumeralSystem/newNumeralSystem.ts
+++ 100AlgorithmsChallenge/newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,19 @@
 function newNumeralSystem(number: string): string[] {
 
+    const numerals = []
+    let startCharCount = 65
+    let endCharCount = number.charCodeAt(0)
+
+    while(startCharCount <= endCharCount){
+        const numeral = `${String.fromCharCode(startCharCount)} + ${String.fromCharCode(endCharCount)}`
+
+        numerals.push(numeral)
+        startCharCount++
+        endCharCount--
+    }
+
+    return numerals
+
 }   
 
 console.log(newNumeralSystem('G'));
