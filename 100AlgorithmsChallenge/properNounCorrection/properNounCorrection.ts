416d0e79025772c74af34f29ac2ff18345b260c5
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-21 05:47:49 -0400

    proper noun correction

--- 100AlgorithmsChallenge/properNounCorrection/properNounCorrection.ts
+++ 100AlgorithmsChallenge/properNounCorrection/properNounCorrection.ts
@@ -1,5 +1,11 @@
 function properNounCorrection(noun: string): string {
    
+    let lower = noun.toLowerCase()
+    let capital = lower.slice(0,1).toUpperCase()
+
+    return capital + lower.slice(1)
+
+    console.log(capital)
 }
 
 console.log(properNounCorrection('pARiS'));
