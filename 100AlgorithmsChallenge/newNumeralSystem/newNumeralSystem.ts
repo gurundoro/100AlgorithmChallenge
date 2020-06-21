8ad9da1d38f466cbe920c06024d0276d64518e90
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 22:00:00 -0400

    new nuemerals with comments

--- 100AlgorithmsChallenge/newNumeralSystem/newNumeralSystem.ts
+++ 100AlgorithmsChallenge/newNumeralSystem/newNumeralSystem.ts
@@ -1,17 +1,25 @@
 function newNumeralSystem(number: string): string[] {
-
+    
+    //create a numeral arrays
     const numerals = []
+    //start character count is 65 for a
     let startCharCount = 65
+    //end character count is the letter in the argument
     let endCharCount = number.charCodeAt(0)
-
+    
+    //create a while loop from either end until the meet each other
     while(startCharCount <= endCharCount){
+        //create string literal using string + charfrom
         const numeral = `${String.fromCharCode(startCharCount)} + ${String.fromCharCode(endCharCount)}`
-
+        
+        //push numeral into array
         numerals.push(numeral)
+        //increase start decrease end
         startCharCount++
         endCharCount--
     }
-
+  
+    //return array
     return numerals
 
 }   
