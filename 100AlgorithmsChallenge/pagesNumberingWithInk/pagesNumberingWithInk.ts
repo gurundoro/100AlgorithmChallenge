5526364ef8a612f94b87619384a30bfb15052ec9
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-06-20 22:11:12 -0400

    pages Numbering with ink

--- 100AlgorithmsChallenge/pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ 100AlgorithmsChallenge/pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,3 +1,10 @@
 function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
+    while(numberOfDigits >= current.toString().length){
+        numberOfDigits -= current.toString().length
+
+        ifumberOfDigits >= current.toString().length){
+            current++
+        }
+    }
 
 }
